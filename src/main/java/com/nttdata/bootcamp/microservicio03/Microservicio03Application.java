f38f2504efc0f75104b01ec46f9b7caf81ddde88
Edward Joseph Valladares Tavera <evalladarestav@gmail.com>
2024-11-18 22:44:51 -0500

    Initial base project

---
+++ src/main/java/com/nttdata/bootcamp/microservicio03/Microservicio03Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.microservicio03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicio03Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Microservicio03Application.class, args);
+  }
+
+}
---
+++ src/test/java/com/nttdata/bootcamp/microservicio03/Microservicio03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.microservicio03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicio03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
